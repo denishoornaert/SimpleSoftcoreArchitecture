ba3e9df3f64cd854683e2b44da84d72539510d57
Denis <dhoornae@ulb.ac.be>
2018-06-24 15:56:15 +0200

    Corrections

--- src/alu/divider/divider.vhd
+++ src/alu/divider/divider.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
-entity multiplier is
+entity divider is
     Port (
         mode     : IN  std_logic_vector(0 to 1); -- "00" : 32bits, "01" : 16bits and "10" : 8bits
         operand1 : IN  std_logic_vector(0 to 31);
@@ -11,9 +11,9 @@ entity multiplier is
         result   : OUT std_logic_vector(0 to 31);
         carries  : OUT std_logic_vector(0 to 3)
     );
-end multiplier;
+end divider;
 
-architecture arch of multiplier is
+architecture arch of divider is
     
 begin
     result  <= (others => '0');
