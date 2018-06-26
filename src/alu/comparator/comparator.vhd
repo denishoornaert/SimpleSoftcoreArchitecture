927be0845306f74082151ed074d0be58e34c4973
Denis <dhoornae@ulb.ac.be>
2018-06-26 19:15:31 +0200

    Take into account the signed integers

--- src/alu/comparator/comparator.vhd
+++ src/alu/comparator/comparator.vhd
@@ -2,14 +2,17 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_MISC.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity comparator is
     Port (
         operand1    : IN  std_logic_vector(0 to 31);
         operand2    : IN  std_logic_vector(0 to 31);
-        equalFlag   : OUT std_logic;
-        biggerFlag  : OUT std_logic;
-        smallerFlag : OUT std_logic
+        eqFlag      : OUT std_logic;
+        bgUnFlag    : OUT std_logic;
+        lwUnFlag    : OUT std_logic;
+        bgSnFlag    : OUT std_logic;
+        lwSnFlag    : OUT std_logic
     );
 end comparator;
 
@@ -18,28 +21,40 @@ architecture arch of comparator is
     signal intermediateBiggerResult   : std_logic_vector(0 to 31);
     signal intermediateSmallerResult  : std_logic_vector(0 to 31);
     signal resultEqual                : std_logic;
-    signal resultBigger               : std_logic;
+    signal resultUnsignedBigger       : std_logic;
+    signal resultSignedBigger         : std_logic;
+    signal intermediateSubtractionRes : std_logic_vector(0 to 31);
 
 begin
     equal: for i in 0 to 31 generate
         intermediateEqualityResult(i) <= operand1(i) xnor operand2(i);
     end generate equal;
-    equalFlag <= and_reduce(intermediateEqualityResult);
+    eqFlag <= and_reduce(intermediateEqualityResult);
     resultEqual <= and_reduce(intermediateEqualityResult);
     
     bigger: for i in 0 to 31 generate
-        eq32: if i=0 generate
+        eqzero: if i=0 generate
             intermediateBiggerResult(i) <= operand1(i) and (not operand2(i));
-        end generate eq32;
+        end generate eqzero;
         otherwise: if i/=0 generate
             intermediateBiggerResult(i) <= (operand1(i) and (not operand2(i))) and and_reduce(intermediateEqualityResult(0 to i-1));
         end generate otherwise;
     end generate bigger;
-    biggerFlag <= or_reduce(intermediateBiggerResult);
-    resultBigger <= or_reduce(intermediateBiggerResult);
+    bgUnFlag <= or_reduce(intermediateBiggerResult);
+    resultUnsignedBigger <= or_reduce(intermediateBiggerResult);
     
-    smallerFlag <= resultEqual nor resultBigger;
-
+    intermediateSubtractionRes <= operand1 + operand2;
+    resultSignedBigger <= (not intermediateSubtractionRes(0)) and (or_reduce(intermediateSubtractionRes(1 to 31))); -- strictly bigger than zero -> msb to 0 and there exists at least one 1
     
+    lwUnFlag <= resultEqual nor resultUnsignedBigger;
+    
+    with resultEqual select
+        eqFlag <= '1' when '1',
+                  '0' when others;
+    with resultEqual select
+        bgSnFlag <= '0' when '1',
+                  resultSignedBigger when others;
+                  
+    lwSnFlag <= not resultSignedBigger;
 
 end arch;
--- test/alu/comparator/comparator_tb.vhd
+++ test/alu/comparator/comparator_tb.vhd
@@ -9,39 +9,45 @@ architecture Behavioral of comparator_tb is
     component comparator port(
         operand1    : IN  std_logic_vector(0 to 31);
         operand2    : IN  std_logic_vector(0 to 31);
-        equalFlag   : OUT std_logic;
-        biggerFlag  : OUT std_logic;
-        smallerFlag : OUT std_logic
+        eqFlag      : OUT std_logic;
+        bgUnFlag    : OUT std_logic;
+        lwUnFlag    : OUT std_logic;
+        bgSnFlag    : OUT std_logic;
+        lwSnFlag    : OUT std_logic
     );
     end component;
     
     type list is array (0 to 7) of std_logic_vector(0 to 31);
     type bits is array (0 to 7) of std_logic;
     
-    signal internalOperand1    : std_logic_vector(0 to 31);
-    signal internalOperand2    : std_logic_vector(0 to 31);
-    signal internalEqualFlag   : std_logic;
-    signal internalBiggerFlag  : std_logic;
-    signal internalSmallerFlag : std_logic;
+    signal intOperand1            : std_logic_vector(0 to 31);
+    signal intOperand2            : std_logic_vector(0 to 31);
+    signal intEqualFlag           : std_logic;
+    signal intBiggerUnsignedFlag  : std_logic;
+    signal intSmallerUnsignedFlag : std_logic;
+    signal intBiggerSignedFlag    : std_logic;
+    signal intSmallerSignedFlag   : std_logic;
 
 begin
-    tbcomparator : comparator port map (operand1=>internalOperand1, operand2=>internalOperand2, equalFlag=>internalEqualFlag, biggerFlag=>internalBiggerFlag, smallerFlag=>internalSmallerFlag);
+    tbcomparator : comparator port map (operand1=>intOperand1, operand2=>intOperand2, eqFlag=>intEqualFlag, bgUnFlag=>intBiggerUnsignedFlag, lwUnFlag=>intSmallerUnsignedFlag, bgSnFlag=>intBiggerSignedFlag, lwSnFlag=>intSmallerSignedFlag);
     
     stimuli : process
             -- inputs
             variable ops1    : list := (X"0000000A", X"FFFFFFFF", X"FFFFFFFF", X"00000F00", X"01010101", X"e0f72f38", X"5c133e99", X"3fbc6412");
             variable ops2    : list := (X"00000000", X"00000000", X"00000001", X"00000F00", X"01010101", X"054deacf", X"faa214eb", X"3f206a5b");
             -- answers
-            variable eqFlag : bits := ('0', '0', '0', '1', '1', '0', '0', '0');
-            variable bgFlag : bits := ('1', '1', '1', '0', '0', '1', '0', '1');
-            variable smFlag : bits := ('0', '0', '0', '0', '0', '0', '1', '0');
+            variable eqFlag  : bits := ('0', '0', '0', '1', '1', '0', '0', '0');
+            variable bgUFlag : bits := ('1', '1', '1', '0', '0', '1', '0', '1');
+            variable lwUFlag : bits := ('0', '0', '0', '0', '0', '0', '1', '0');
+            variable bgSFlag : bits := ('1', '0', '0', '0', '0', '0', '1', '1');
+            variable lwSFlag : bits := ('0', '1', '1', '0', '0', '1', '0', '0');
             
         begin
             for i in 0 to 7 loop
-                internalOperand1 <= ops1(i);
-                internalOperand2 <= ops2(i);
+                intOperand1 <= ops1(i);
+                intOperand2 <= ops2(i);
                 wait for 125 ns;
-                assert ((internalEqualFlag = eqFlag(i)) and (internalBiggerFlag = bgFlag(i)) and (internalSmallerFlag = smFlag(i))) report "Error !";
+                assert ((intEqualFlag = eqFlag(i)) and (intBiggerUnsignedFlag = bgUFlag(i)) and (intSmallerUnsignedFlag = lwUFlag(i)) and (intBiggerSignedFlag = bgSFlag(i)) and (intSmallerUnsignedFlag = lwUFlag(i))) report "Error !";
             end loop;
         end process;
 
